6a4fa4fa9d2260c627daa53f2fc4c88acea2ea0d
ChuHoYeon <158002841+ChuHoYeon@users.noreply.github.com>
2026-05-15 22:48:43 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/freelecwebservice/FreelecWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.freelecwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelecWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/freelecwebservice/FreelecWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freelecwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
