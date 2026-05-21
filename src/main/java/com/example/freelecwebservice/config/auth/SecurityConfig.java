5f21b24a33a5dba68137b73fab30a7c6e5426569
ChuHoYeon <158002841+ChuHoYeon@users.noreply.github.com>
2026-05-21 14:09:45 +0900

    Feat: OAuth2 로그인 설정 추가

---
+++ src/main/java/com/example/freelecwebservice/config/auth/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.freelecwebservice.config.auth;
+
+import com.example.freelecwebservice.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.security.autoconfigure.web.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(PathRequest.toH2Console())
+                        .ignoringRequestMatchers("/api/v1/**")
+                )
+                .cors(AbstractHttpConfigurer::disable)
+                .headers(header -> header
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                        .requestMatchers("/api/v1/**").hasRole(Role.USER.name())
+                        .anyRequest().authenticated()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                )
+                .oauth2Login(oauth2 -> oauth2.
+                        userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)
+                        )
+                );
+
+        return http.build();
+    }
+}
