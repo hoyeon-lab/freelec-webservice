63c1c2fcac7d7aa8fbf71dbaf65aba3030fa2eae
ChuHoYeon <158002841+ChuHoYeon@users.noreply.github.com>
2026-05-21 14:10:21 +0900

    Feat: 로그인 사용자 화면 연동

---
+++ src/main/java/com/example/freelecwebservice/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,31 @@
+package com.example.freelecwebservice.config.auth;
+
+import com.example.freelecwebservice.config.auth.dto.SessionUser;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    @Override
+    public @Nullable Object resolveArgument(MethodParameter parameter, @Nullable ModelAndViewContainer mavContainer, NativeWebRequest webRequest, @Nullable WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
+}
--- src/main/java/com/example/freelecwebservice/controller/IndexController.java
+++ src/main/java/com/example/freelecwebservice/controller/IndexController.java
@@ -1,7 +1,10 @@
 package com.example.freelecwebservice.controller;
 
+import com.example.freelecwebservice.config.auth.LoginUser;
+import com.example.freelecwebservice.config.auth.dto.SessionUser;
 import com.example.freelecwebservice.dto.PostsResponseDto;
 import com.example.freelecwebservice.service.PostsService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,10 +16,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
