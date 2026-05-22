e8c7d7c76c61470fed21877d5cda05d8fed45da0
ChuHoYeon <158002841+ChuHoYeon@users.noreply.github.com>
2026-05-22 11:54:45 +0900

    Chore: H2 콘솔 보안 설정 정리

--- src/main/java/com/example/freelecwebservice/config/auth/SecurityConfig.java
+++ src/main/java/com/example/freelecwebservice/config/auth/SecurityConfig.java
@@ -19,15 +19,15 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf
-                        .ignoringRequestMatchers(PathRequest.toH2Console())
+//                        .ignoringRequestMatchers(PathRequest.toH2Console())
                         .ignoringRequestMatchers("/api/v1/**")
                 )
                 .cors(AbstractHttpConfigurer::disable)
-                .headers(header -> header
-                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
-                )
+//                .headers(header -> header
+//                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+//                )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+//                        .requestMatchers(PathRequest.toH2Console()).permitAll()
                         .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
                         .requestMatchers("/api/v1/**").hasRole(Role.USER.name())
                         .anyRequest().authenticated()
